70b9e1fc4d85f3014cd37a512bed09cfa1ec7b25
celeste <92414500+cetigerlily@users.noreply.github.com>
2022-08-26 00:15:57 +0800

    Add files via upload

---
+++ labs/lab1/src/PeaSoup.java
@@ -0,0 +1,46 @@
+/**
+ * Celeste Tigerlily Cheah Kae
+ * A0245928R
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PeaSoup {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int numberOfRests = Integer.parseInt(s.nextLine());
+        ArrayList<String> rests = new ArrayList<>();
+ 
+        while(numberOfRests > 0) { 
+            int numberOfItems = Integer.parseInt(s.nextLine());
+            String restName = s.nextLine();
+
+            boolean peaSoup = false;
+            boolean pancakes = false;
+
+            while(numberOfItems > 0) {
+                String currentItem = s.nextLine();
+                if(currentItem.equals("pea soup")) {
+                    peaSoup = true;
+                } else if (currentItem.equals("pancakes")) {
+                    pancakes = true;
+                }
+                if(peaSoup && pancakes) {
+                    rests.add(restName);
+                    break;
+                }
+                numberOfItems--;
+            }
+            numberOfRests--;
+        }
+
+        if(rests.isEmpty()) {
+            System.out.println("Anywhere is fine I guess");
+        } else {
+            for(int c = 0; c < rests.size(); c++) {
+                System.out.println(rests.get(c));
+            }
+        }
+    }
+}
