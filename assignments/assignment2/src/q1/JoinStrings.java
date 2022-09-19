8e89e3b42da3d0a782a3b7f9ddeebf44bb99ed77
celeste <92414500+cetigerlily@users.noreply.github.com>
2022-09-19 23:52:39 +0800

    update

--- assignments/assignment2/src/q1/JoinStrings.java
+++ assignments/assignment2/src/q1/JoinStrings.java
@@ -6,16 +6,6 @@ package q1;
 
 import java.util.*;
 import java.lang.*;
-
-public class JoinStrings {
-    public static void main(String[] args) throws Exception {apackage q1;
-/**
- * Celeste Tigerlily Cheah Kae
- * A0245928R
- */
-
-import java.util.*;
-import java.lang.*;
 import java.io.*;
 
 public class JoinStrings {
@@ -107,32 +97,5 @@ class Kattio extends PrintWriter {
 	    String ans = peekToken();
 	    token = null;
 	    return ans;
-    }
-}
-        Scanner s = new Scanner(System.in);
-        int numOfWords = Integer.parseInt((s.nextLine()));
-
-        ArrayList<String> words = new ArrayList<>();
-        for(int i = 0; i < numOfWords; i++) {
-            String input = s.nextLine();
-            words.add(input);
-        }
-
-        for(int j = 0; j < numOfWords - 1; j++) {
-            StringBuilder result = new StringBuilder();
-            int aString = s.nextInt();
-            int bString = s.nextInt();
-            result.append(words.get(aString - 1));
-            result.append(words.get(bString - 1));
-
-            words.set(aString - 1, result.toString());
-            words.set(bString - 1, "");
-        }
-    
-        for(int k = 0; k < words.size(); k++) {
-            if(words.get(k) != "") {
-                System.out.println(words.get(k));
-            }
-        }
     }
 }
