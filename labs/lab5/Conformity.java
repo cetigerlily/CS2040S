0556f4a090196c2553f2488a57f8b480fc41ef3e
celeste <92414500+cetigerlily@users.noreply.github.com>
2022-09-29 15:49:07 +0800

    update

--- labs/lab5/Conformity.java
+++ labs/lab5/Conformity.java
@@ -1,55 +1,54 @@
 /**
- * Pseudo-code:
- * 1) for each frosh, put it's class combo into a hash-map
- * 2) for each subsequent frosh, compare all five courses at a time
- *    a) if no difference, popularity++ (store popularity)
- *    b) if find any difference, break and move on to next frosh
- * 3) have a tracker to track highest popularity, output
+ * Celeste Tigerlily Cheah Kae
+ * A0245928R
  */
- 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 public class Conformity {
-    Kattio io = new Kattio(System.in, System.out);
-    int numOfFrosh = io.getInt();
-    
-    for(int i = 0; i < numOfFrosh; i++) {
-        String courses = io.getLine();
-        // split the input of courses
-        addCourse(0, /* course 1 */);
-        addCourse(1, /* course 2 */);
-    }
-    
-    for(int i = 0; i < numOfFrosh; i++) {
-        for(int j = 0; j < numOfFrosh - 1; j++) {
-            if(i != j) {
-                /* compare all the courses */ 
+    public static void main(String[] args) {
+        Kattio io = new Kattio(System.in, System.out);
+        int numOfFrosh = Integer.parseInt((io.getWord()));
+        HashMap<HashSet<Integer>, Integer> allCC = new HashMap<>(); // <frosh's CC, popularity = number of same CC>
+        
+        HashSet<Integer> mostPopularCC = new HashSet<>(); // CC which is the most popular
+        int numOfPopularCC = 0; // number of CC which are equally popular
+
+        for(int i = 0; i < numOfFrosh; i++) {
+            HashSet<Integer> thisFrosh = new HashSet<>();
+            for(int j = 0; j < 5; j++) {
+                thisFrosh.add(io.getInt());
+                if(j == 0) {
+                    mostPopularCC = thisFrosh; // initialize first CC to be the most popular
+                }
+            }
+
+            if(allCC.containsKey(thisFrosh)) {
+                allCC.put(thisFrosh, allCC.get(thisFrosh) + 1);
+
+                if(allCC.get(mostPopularCC) < allCC.get(thisFrosh)) { // if popularity of most popular CC < popularity of this frosh
+                    mostPopularCC = thisFrosh;
+                    numOfPopularCC = 1;
+                } else if (allCC.get(mostPopularCC) == allCC.get(thisFrosh)) { // have the same popularities
+                    numOfPopularCC += 1;
+                }
+
+            } else {
+                allCC.put(thisFrosh, 1);
             }
         }
-    }
-    
-}
 
-class Frosh {
-    private HashMap<Integer, Integer> courses;
-    private int popularity;
-    
-    public Frosh() {
-        this.courses = new HashMap<Integer, Integer>();
-        this.popularity = popularity;
-    }
-    
-    public int getPopularity() {
-        return this.popularity;
-    }
-    
-    public void addCourse(int i, int course) {
-        courses.put(i, course);
-    }
-    
-    public int getCourse(int i) {
-        courses.get(i);
-    }
-    
-    /* make the frosh a comparable */
-    public boolean compareTo
+        if(numOfPopularCC == 0) { // all CC have popularity of 1
+            io.println(numOfFrosh);
+        } else if(numOfPopularCC == 1) { // one CC is the most popular
+            io.println(allCC.get(mostPopularCC));
+        } else { // when numOfPopularCC > 1, could either be n different popular CCs or all of the CCs are the same
+            int maxPopularity = Collections.max(allCC.values());
+            int frequency = Collections.frequency(allCC.values(), maxPopularity);
+            io.println(maxPopularity * frequency);
+        }
+        
+        io.flush();
+    }   
 }
