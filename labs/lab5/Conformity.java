17ef20439551f073bf8f6379217062d62c15cf42
celeste <92414500+cetigerlily@users.noreply.github.com>
2022-09-29 13:25:45 +0800

    update

--- labs/lab5/Conformity.java
+++ labs/lab5/Conformity.java
@@ -6,3 +6,50 @@
  *    b) if find any difference, break and move on to next frosh
  * 3) have a tracker to track highest popularity, output
  */
+ 
+
+public class Conformity {
+    Kattio io = new Kattio(System.in, System.out);
+    int numOfFrosh = io.getInt();
+    
+    for(int i = 0; i < numOfFrosh; i++) {
+        String courses = io.getLine();
+        // split the input of courses
+        addCourse(0, /* course 1 */);
+        addCourse(1, /* course 2 */);
+    }
+    
+    for(int i = 0; i < numOfFrosh; i++) {
+        for(int j = 0; j < numOfFrosh - 1; j++) {
+            if(i != j) {
+                /* compare all the courses */ 
+            }
+        }
+    }
+    
+}
+
+class Frosh {
+    private HashMap<Integer, Integer> courses;
+    private int popularity;
+    
+    public Frosh() {
+        this.courses = new HashMap<Integer, Integer>();
+        this.popularity = popularity;
+    }
+    
+    public int getPopularity() {
+        return this.popularity;
+    }
+    
+    public void addCourse(int i, int course) {
+        courses.put(i, course);
+    }
+    
+    public int getCourse(int i) {
+        courses.get(i);
+    }
+    
+    /* make the frosh a comparable */
+    public boolean compareTo
+}
