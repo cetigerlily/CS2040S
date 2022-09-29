87dc6cf034fd8e2da1d3127b7c91945df7011096
celeste <92414500+cetigerlily@users.noreply.github.com>
2022-09-29 12:53:09 +0800

    upload

---
+++ labs/lab5/Conformity.java
@@ -0,0 +1,8 @@
+/**
+ * Pseudo-code:
+ * 1) for each frosh, put it's class combo into a hash-map
+ * 2) for each subsequent frosh, compare all five courses at a time
+ *    a) if no difference, popularity++ (store popularity)
+ *    b) if find any difference, break and move on to next frosh
+ * 3) have a tracker to track highest popularity, output
+ */
