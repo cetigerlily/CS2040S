c1843616dd21a8c52a8e278ec8d567ae0e4d2241
celeste <92414500+cetigerlily@users.noreply.github.com>
2022-10-27 16:25:27 +0800

    upload

---
+++ labs/lab8/Land.java
@@ -0,0 +1,17 @@
+public class Land {
+    private int r;
+    private int c;
+
+    public Land(int r, int c) {
+        this.r = r;
+        this.c = c;
+    }
+
+    public int getR() {
+        return this.r;
+    }
+
+    public int getC() {
+        return this.c;
+    }
+}
