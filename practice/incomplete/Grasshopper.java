e13b1abe4a0e45f0f9c2612a407bbd33ced008ce
celeste <92414500+cetigerlily@users.noreply.github.com>
2022-11-02 00:38:57 +0800

    update

--- practice/incomplete/Grasshopper.java
+++ practice/incomplete/Grasshopper.java
@@ -1,53 +1,71 @@
 import java.util.*;
-import java.io.*;
-import java.lang.*;
-import java.security.cert.PolicyQualifierInfo;
 import java.awt.*;
 
 public class Grasshopper {
+    static int R;
+    static int C;
+    static Queue<Vertex> queue;
+    static boolean visited[][];
+    static int[] moveX = {-1, -2, -2, -1, 1, 2, 2, 1};
+    static int[] moveY = {2, 1, -1, -2, -2, -1, 1, 2};
+
+    public static void BFS(Vertex input) {
+        for(int i = 0; i < 8; i++) {
+            int newX = (int) input.getPoint().getX() + moveX[i];
+            int newY = (int) input.getPoint().getY() + moveY[i];
+
+            if(newX >= 0 && newX < C && newY >= 0 && newY < R) {
+                if(!visited[newX][newY]) {
+                    visited[newX][newY] = true;
+                    queue.add(new Vertex(new Point(newX, newY), input.getDistance() + 1));
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Kattio io = new Kattio(System.in, System.out);
 
         while(io.hasMoreTokens()) {
-            int R = io.getInt();
-            int C = io.getInt();
-            int gR = io.getInt();
-            int gC = io.getInt();
-            int lR = io.getInt();
-            int lC = io.getInt();
+            R = io.getInt();
+            C = io.getInt();
+            int gR = io.getInt() - 1;
+            int gC = io.getInt() - 1;
+            int lR = io.getInt() - 1;
+            int lC = io.getInt() - 1;
     
-            int index = 0;
-            int vertex[][] = new int[R][C];
-            for(int i = 1; i <= R; i++) {
-                for(int j = 1; j <= C; j++) {
-                    vertex[i][j] = index;
-                    index += 1;
-                }
-            }
-    
-            int distance[][] = new int[R][C]; // distance array from source to a vertex ie. distance[i][j] = distance of vertex[i][j] to source
-            int previous[][] = new int[R][C]; // previous vertex
-    
-            Queue<Integer> queue = new LinkedList<>();
-
+            visited = new boolean[R][C];
             for(int i = 0; i < R; i++) {
                 for(int j = 0; j < C; j++) {
-                    distance[i][j] = Integer.MAX_VALUE;
-                    previous[i][j] = -1;
-                    queue.add(vertex[i][j]);
+                    visited[i][j] = false;
                 }
             }
 
-            distance[gR][gC] = 0;
+            Point source = new Point(gR, gC);
+            Point leaf = new Point(lR, lC);
 
+            queue = new LinkedList<>();
+            queue.add(new Vertex(source, 0));
+            visited[gR][gC] = true;
+
+            boolean reachedLeaf = false;
+            int numberOfHops = 0;
             while(!queue.isEmpty()) {
-                int current = queue.poll();
-                if(current == vertex[lR][lC]) {
+                Vertex current = queue.poll();
+                if(current.getPoint().equals(leaf)) {
+                    reachedLeaf = true;
+                    numberOfHops = current.getDistance();
                     break;
                 }
-                BFS(current); // search for each neighbour v of current in Q
+                BFS(current);
+            }
+
+            if(reachedLeaf) {
+                io.println(numberOfHops);
+            } else {
+                io.println("impossible");           
             }
         }
-        io.flush();
+        io.close();
     }
 }
