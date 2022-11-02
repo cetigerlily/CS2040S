65341353f74bfdb5269a8bed74b3f9e1f1ef06d5
celeste <92414500+cetigerlily@users.noreply.github.com>
2022-11-02 13:38:18 +0800

    update

--- practice/incomplete/Grasshopper/Grasshopper.java
+++ practice/incomplete/Grasshopper/Grasshopper.java
@@ -1,29 +1,13 @@
 import java.util.*;
 import java.awt.*;
+import java.io.*;
 
 public class Grasshopper {
-    static int R;
-    static int C;
-    static Queue<Vertex> queue;
-    static boolean visited[][];
-    static int[] moveX = {-1, -2, -2, -1, 1, 2, 2, 1};
-    static int[] moveY = {2, 1, -1, -2, -2, -1, 1, 2};
-
-    public static void BFS(Vertex input) {
-        for(int i = 0; i < 8; i++) {
-            int newX = (int) input.getPoint().getX() + moveX[i];
-            int newY = (int) input.getPoint().getY() + moveY[i];
-
-            if(newX >= 0 && newX < C && newY >= 0 && newY < R) {
-                if(!visited[newX][newY]) {
-                    visited[newX][newY] = true;
-                    queue.add(new Vertex(new Point(newX, newY), input.getDistance() + 1));
-                }
-            }
-        }
-    }
+    static int[] moveX;
+    static int[] moveY;
 
     public static void main(String[] args) {
+        moves();
         Kattio io = new Kattio(System.in, System.out);
 
         while(true) {
@@ -31,14 +15,14 @@ public class Grasshopper {
                 break;
             }
 
-            R = io.getInt();
-            C = io.getInt();
+            int R = io.getInt();
+            int C = io.getInt();
             int gR = io.getInt() - 1;
             int gC = io.getInt() - 1;
             int lR = io.getInt() - 1;
             int lC = io.getInt() - 1;
     
-            visited = new boolean[R][C];
+            boolean[][] visited = new boolean[R][C];
             for(int i = 0; i < R; i++) {
                 for(int j = 0; j < C; j++) {
                     visited[i][j] = false;
@@ -48,28 +32,63 @@ public class Grasshopper {
             Point source = new Point(gR, gC);
             Point leaf = new Point(lR, lC);
 
-            queue = new LinkedList<>();
+            Queue<Vertex> queue = new LinkedList<>();
             queue.add(new Vertex(source, 0));
             visited[gR][gC] = true;
 
             boolean reachedLeaf = false;
             int numberOfHops = 0;
+
             while(!queue.isEmpty()) {
                 Vertex current = queue.poll();
+
                 if(current.getPoint().equals(leaf)) {
                     reachedLeaf = true;
                     numberOfHops = current.getDistance();
                     break;
                 }
-                BFS(current);
+
+                for(int i = 0; i < 8; i++) {
+                    int newX = (int) current.getPoint().getX() + moveX[i];
+                    int newY = (int) current.getPoint().getY() + moveY[i];
+        
+                    if((newX >= 0) && (newX < C) && (newY >= 0) && (newY < R) && (!visited[newX][newY])) {
+                        visited[newX][newY] = true;
+                        queue.add(new Vertex(new Point(newX, newY), current.getDistance() + 1));
+                    }
+                }
             }
+            
 
             if(reachedLeaf) {
                 io.println(numberOfHops);
-            } else {
+            } else if(!reachedLeaf) {
                 io.println("impossible");           
             }
         }
         io.close();
+    }
+
+    public static void moves() {
+        moveX = new int[]{-1, -2, -2, -1, 1, 2, 2, 1};
+        moveY = new int[]{2, 1, -1, -2, -2, -1, 1, 2};
+    }
+}
+
+class Vertex {
+    private Point point;
+    private int distance;
+
+    public Vertex(Point point, int distance) {
+        this.point = point;
+        this.distance = distance;
+    }
+
+    public Point getPoint() {
+        return this.point;
+    }
+
+    public int getDistance() {
+        return this.distance;
     }
 }
